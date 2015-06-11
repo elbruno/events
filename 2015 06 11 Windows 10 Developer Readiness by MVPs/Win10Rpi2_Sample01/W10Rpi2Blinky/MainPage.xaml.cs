2b5502a5d654798de8c660ac9d0d246d3cd4b2ad
El Bruno <bcapuano@gmail.com>
2015-06-11 17:27:46 +0200

    +add blinky sample

---
+++ 2015 06 11 Windows 10 Developer Readiness by MVPs/Win10Rpi2_Sample01/W10Rpi2Blinky/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Diagnostics;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+
+namespace W10Rpi2Blinky
+{
+    public sealed partial class MainPage
+    {
+        private DispatcherTimer _timerBlink;
+
+        public MainPage()
+        {
+            InitGpioController();
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            _timerBlink = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(1000) };
+            _timerBlink.Tick += _timerBlink_Tick;
+            _timerBlink.Start();
+        }
+
+        private GpioPin _pin;
+        private bool _ledStatus;
+
+        private void _timerBlink_Tick(object sender, object e)
+        {
+            TextBlockStatus.Text = $"Led Status is {_ledStatus}";
+            _pin?.Write(_ledStatus ? GpioPinValue.High : GpioPinValue.Low);
+            _ledStatus = !_ledStatus;
+        }
+
+        private void InitGpioController()
+        {
+            var gpio = GpioController.GetDefault();
+            if (gpio == null)
+            {
+                _pin = null;
+                return;
+            }
+
+            GpioOpenStatus openstatus;
+            gpio.TryOpenPin(LedPin, GpioSharingMode.Exclusive, out _pin, out openstatus);
+            Debug.WriteLine("pin: " + LedPin + " status: " + openstatus);
+
+            if (_pin == null)
+            {
+                return;
+            }
+
+            _pin.Write(GpioPinValue.High);
+            _pin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+        private const int LedPin = 6;
+
+    }
+}
--- 2015 06 11 Windows 10 Developer Readiness by MVPs/Win10Rpi2_Sample01/Win10Rpi2_Sample01/MainPage.xaml.cs
+++ 2015 06 11 Windows 10 Developer Readiness by MVPs/Win10Rpi2_Sample01/Win10Rpi2_Sample01/MainPage.xaml.cs
@@ -1,17 +1,30 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace Win10Rpi2_Sample01
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private DispatcherTimer _timer;
+
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //_timer = new DispatcherTimer();
+            //_timer.Interval = TimeSpan.FromSeconds(1);
+            //_timer.Tick += _timer_Tick;
+            //_timer.Start();
+        }
+
+        private void _timer_Tick(object sender, object e)
+        {
+            TextBlockMessage.Text = DateTime.Now.ToString();
         }
     }
 }
