1b22e4d2f14eb15bda128abb88084dc2c54ed38f
El Bruno <bcapuano@gmail.com>
2015-07-07 19:10:32 +0200

    display battery level
    display chart with hear rate beats

--- 2015 07 04 Gap Andorra/06Ble/WuaBleHr01/MainPage.xaml.cs
+++ 2015 07 04 Gap Andorra/06Ble/WuaBleHr01/MainPage.xaml.cs
@@ -1,44 +1,159 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
+using System.Threading.Tasks;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Devices.Enumeration;
 using Windows.Storage.Streams;
 using Windows.UI.Core;
+using Windows.UI.Xaml;
+using WinRTXamlToolkit.Controls.DataVisualization.Charting;
 using WuaBleHr01.Annotations;
+using WuaBleHr01.Model;
 
 namespace WuaBleHr01
 {
     public sealed partial class MainPage : INotifyPropertyChanged
     {
-        private DeviceInformation _devicePolar;
+        private DeviceInformation _devicePolarHr;
+        private DeviceInformation _devicePolarBattery;
+        DispatcherTimer _timer = new DispatcherTimer();
+
         public MainPage()
         {
             InitializeComponent();
             DataContext = this;
+            HrCollection = new List<HrModel>();
             Loaded += MainPage_Loaded;
         }
 
-        private async void MainPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
         {
-            
-            var devices = await DeviceInformation.FindAllAsync(); // .FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.HeartRate));
-            if (null == devices || devices.Count <= 0) return;
-            //foreach (var device in devices.Where(device => device.Name == "Polar H7 498C1817"))
-            foreach (var device in devices.Where(device => device.Name == "Charge HR"))
 
+            _timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+            ButtonLoad.Visibility = Visibility.Collapsed;
+        }
+
+        private async void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (DeviceFound())
             {
-                _devicePolar = device;
-                StatusInformation = string.Format("Found {0}", _devicePolar.Name);
-                break;
+                await SuscribeToHrValues();
+                await DisplayBatteryLevel();
+                DeviceFoundMessage = $@"[{_devicePolarHr.Name}]
+Connected";
+                ButtonLoad.Content = "Disconnect ...";
+            }
+            else
+            {
+                ButtonLoad.Content = "Connect ...";
+                _devicePolarHr = null;
+                _devicePolarBattery = null;
+            }
+        }
+
+        private async void _timer_Tick(object sender, object e)
+        {
+            if (DeviceFound()) return;
+
+            DeviceFoundMessage = "Searching ...";
+            var found = await GetHrAndBatteryDevice();
+            if (!found) return;
+            DeviceFoundMessage = $@"[{_devicePolarHr.Name}]";
+            ButtonLoad.Visibility = Visibility.Visible;
+        }
+
+        private async void DisplayHrData()
+        {
+            var myList = new List<HrModel>();
+            for (var i = 0; i < 100; i++)
+            {
+                var hr = new HrModel { Time = i };
+                if (HrCollection.Count > i)
+                    hr.HeartRate = HrCollection[i].HeartRate;
             }
 
-            if (_devicePolar == null) return;
-            var service = await GattDeviceService.FromIdAsync(_devicePolar.Id);
-            if (null == service ) return;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                _lineSeries = LineChart.Series[0] as LineSeries;
+                if (_lineSeries != null)
+                    _lineSeries.ItemsSource = myList;
+
+            });
+        }
+
+        private bool DeviceFound()
+        {
+            if (_devicePolarHr != null && _devicePolarBattery != null)
+                return true;
+            return false;
+        }
+
+        private void GenerateSampleData()
+        {
+            var rnd = new Random();
+            var myList = new List<HrModel>();
+
+            var hr = new HrModel
+            {
+                Time = 0,
+                HeartRate = 65
+            };
+            myList.Add(hr);
+            for (var i = 1; i < 100; i++)
+            {
+                hr = new HrModel
+                {
+                    Time = i,
+                    HeartRate = 70 + rnd.Next(5)
+                };
+                myList.Add(hr);
+            }
+            hr = new HrModel
+            {
+                Time = 0,
+                HeartRate = 90
+            };
+            myList.Add(hr);
+
+            _lineSeries = LineChart.Series[0] as LineSeries;
+            if (_lineSeries != null)
+                _lineSeries.ItemsSource = myList;
+        }
+        
+        private async Task DisplayBatteryLevel()
+        {
+            var service = await GattDeviceService.FromIdAsync(_devicePolarBattery.Id);
+            var characteristics = service?.GetAllCharacteristics();
+            if (characteristics == null || characteristics.Count <= 0) return;
+            var characteristic = characteristics[0];
+            try
+            {
+                var batteryLevelValue = await characteristic.ReadValueAsync();
+                var arrayLenght = (int)batteryLevelValue.Value.Length;
+                var batteryData = new byte[arrayLenght];
+                DataReader.FromBuffer(batteryLevelValue.Value).ReadBytes(batteryData);
+
+                // battery level is on first element
+                var batteryLevel = batteryData[0];
+                Debug.WriteLine(batteryLevel);
+                BatteryValue = $"{batteryLevel} %";
+            }
+            catch (Exception exception)
+            {
+                StatusInformation2 = exception.ToString();
+            }
+        }
+
+        private async Task SuscribeToHrValues()
+        {
+            var service = await GattDeviceService.FromIdAsync(_devicePolarHr.Id);
+            if (null == service) return;
 
             var characteristics = service.GetAllCharacteristics();
             if (null == characteristics || characteristics.Count <= 0) return;
@@ -47,18 +162,42 @@ namespace WuaBleHr01
                 try
                 {
                     characteristic.ValueChanged += GattCharacteristic_ValueChanged;
-                    //await characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(GattClientCharacteristicConfigurationDescriptorValue.Notify);
-
-                    var val = await characteristic.ReadValueAsync();
-                    
-
-                    Debug.WriteLine(val.Value.ToString());
+                    await
+                        characteristic.WriteClientCharacteristicConfigurationDescriptorAsync(
+                            GattClientCharacteristicConfigurationDescriptorValue.Notify);
                 }
                 catch (Exception exception)
                 {
-                    StatusInformation2 = exception.ToString();
+                    Debug.WriteLine(exception);
                 }
-            } 
+            }
+        }
+
+        private async Task<bool> GetHrAndBatteryDevice()
+        {
+            StatusInformation = "Start search for devices, HR";
+            var devices = await DeviceInformation.FindAllAsync(
+                GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.HeartRate));
+            if (null == devices || devices.Count <= 0) return true;
+            foreach (var device in devices.Where(device => device.Name == "Polar H7 498C1817"))
+            {
+                _devicePolarHr = device;
+                StatusInformation2 = "Found hr device";
+                break;
+            }
+
+            StatusInformation = "Start search for devices, Battery";
+            devices = await DeviceInformation.FindAllAsync(
+                GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.Battery));
+            if (null == devices || devices.Count <= 0) return true;
+            foreach (var device in devices.Where(device => device.Name == "Polar H7 498C1817"))
+            {
+                _devicePolarBattery = device;
+                StatusInformation2 = "Found battery device";
+                break;
+            }
+            StatusInformation = $"Found HR [{(_devicePolarHr != null)}] Battery [{(_devicePolarBattery != null)}]";
+            return (_devicePolarHr != null && _devicePolarBattery != null);
         }
 
         private void GattCharacteristic_ValueChanged(GattCharacteristic sender, GattValueChangedEventArgs args)
@@ -66,15 +205,18 @@ namespace WuaBleHr01
             if (args == null) return;
             if (args.CharacteristicValue.Length == 0) return;
 
-            int arrayLenght = (int) args.CharacteristicValue.Length;
-            byte[] hrData = new byte[arrayLenght];
+            var arrayLenght = (int)args.CharacteristicValue.Length;
+            var hrData = new byte[arrayLenght];
             DataReader.FromBuffer(args.CharacteristicValue).ReadBytes(hrData);
 
             //Convert to string  
             var hrValue = ProcessData(hrData);
-
             Debug.WriteLine(hrValue);
             HeartRateValue = hrValue.ToString();
+
+            HrCollection.Add(new HrModel { HeartRate = hrValue });
+            DisplayHrData();
+
         }
 
         private int ProcessData(byte[] data)
@@ -99,25 +241,11 @@ namespace WuaBleHr01
             else
             {
                 heartRateMeasurementValue = data[currentOffset];
-                currentOffset++;
             }
 
-              return  heartRateMeasurementValue;
+            return heartRateMeasurementValue;
         }
-
-        private async void LoadBatteryLevel()
-        {
-            var batteryServices = await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(GattServiceUuids.Battery), null);
-
-            if (batteryServices.Count > 0)
-            {
-                foreach (var di in batteryServices)
-                {
-                    Debug.WriteLine(di.Name);
-                }
-            }
-        }
-
+        
         #region Properties and Property Changed
         public event PropertyChangedEventHandler PropertyChanged;
 
@@ -167,6 +295,47 @@ namespace WuaBleHr01
                 OnPropertyChanged();
             }
         }
+
+        private string _batteryValue;
+
+        public string BatteryValue
+        {
+            get { return _batteryValue; }
+            set
+            {
+                if (value == _batteryValue) return;
+                _batteryValue = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private List<HrModel> _hrCollection;
+        private LineSeries _lineSeries;
+
+        public List<HrModel> HrCollection
+        {
+            get { return _hrCollection; }
+            set
+            {
+                if (value == _hrCollection) return;
+                _hrCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _deviceFoundMessage;
+        public string DeviceFoundMessage
+        {
+            get { return _deviceFoundMessage; }
+            set
+            {
+                if (value == _deviceFoundMessage) return;
+                _deviceFoundMessage = value;
+                OnPropertyChanged();
+            }
+        }
+
         #endregion
+
     }
 }
