874c34105fceb60afc864908d15901d5d721f495
El Bruno <elbruno@users.noreply.github.com>
2018-03-22 20:24:29 -0400

    Materials used during the Toronto MetroUG Microsoft AI on 2018 03 21

---
+++ 20180321 Toronto MetroUG Microsoft AI/ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        const string ComputerVisionKey = "< Computer Vision Key goes here ... >";
+        const string CVUriBase = "https://westus.api.cognitive.microsoft.com/vision/v1.0/";
+        const string CVApiMethod = "analyze";
+        const string FileSource = @"< Image File path ... >";
+
+        static void Main(string[] args)
+        {
+            var imageFilePath = FileSource;
+            MakeAnalysisRequest(imageFilePath);
+            Console.ReadLine();
+        }
+
+        public static async void MakeAnalysisRequest(string imageFilePAth)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", ComputerVisionKey);
+
+            var requestParameters = "visualFeatures=Categories,Description,Color&language=en";
+            var uri = CVUriBase + CVApiMethod + "?" + requestParameters;
+            //string uri = CVUriBase + "?" + requestParameters;
+
+            var byteData = GetImageAsByteArray(imageFilePAth);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType =
+                    new System.Net.Http.Headers.MediaTypeHeaderValue("application/octet-stream");
+                var response = await client.PostAsync(uri, content);
+                var contentstring = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("\nResponse:\n");
+                Console.WriteLine(contentstring);
+                Console.WriteLine();
+                //Console.WriteLine(CoolJsonFormat(contentstring));
+            }
+        }
+
+        public static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            var fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            var binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        private static string CoolJsonFormat(string json)
+        {
+            dynamic parsedJson = JsonConvert.DeserializeObject(json);
+            return JsonConvert.SerializeObject(parsedJson, Newtonsoft.Json.Formatting.Indented);
+        }
+    }
+}
+
---
+++ 20180321 Toronto MetroUG Microsoft AI/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Cognitive.LUIS;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        private const string SubscriptionKey = "< LUIS subscription key goes here >";
+        private const string LuisAppId = "< LUIS Application Id goes here >";
+        private const string QueryString = "";
+
+        static void Main(string[] args)
+        {
+            GetLuisUsingLuis();
+            Console.ReadLine();
+        }
+
+       private static async void GetLuisUsingLuis()
+        {
+            var luisClient = new LuisClient(LuisAppId, SubscriptionKey, true);
+            var res = await luisClient.Predict(QueryString);
+            var outputMsg = $@"Intent [{res.Intents.FirstOrDefault().Name}, {res.Intents.FirstOrDefault().Score}] {Environment.NewLine}";
+            foreach (var resEntity in res.Entities)
+            {
+                outputMsg += $@"Entity: [{resEntity.Value.FirstOrDefault().Name}, {resEntity.Value.FirstOrDefault().Score} ] {Environment.NewLine}";
+            }
+            Console.Write(outputMsg);
+        }
+        private static async void  GetLuisIntents()
+        {
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+            queryString["q"] = "myname is bruno capuano. I want to report an accident";
+            queryString["timezoneOffset"] = "0";
+            queryString["verbose"] = "false";
+            queryString["spellCheck"] = "false";
+            queryString["staging"] = "false";
+
+            var uri = "https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/" + LuisAppId + "?" + queryString;
+            var response = await client.GetAsync(uri);
+
+            var strResponseContent = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(strResponseContent);
+        }
+    }
+}
