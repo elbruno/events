e557421868529cd37b73e64bae0addc6e88de468
El Bruno <elbruno@users.noreply.github.com>
2018-08-01 21:15:22 -0400

    add sample code from the C# Community Event

---
+++ 2018 08 01 CSharpComm MLNet/MLNetDemos03ExportModel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using MLNetAvaLL.Shared;
+
+namespace MLNetDemos03ExportModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "AgeRanges.csv";
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader(fileName).CreateFrom<AgeRange>(separator: ',', useHeader: true));
+            pipeline.Add(new Dictionarizer("Label"));
+            pipeline.Add(new TextFeaturizer("Gender", "Gender"));
+            pipeline.Add(new ColumnConcatenator("Features", "Age", "Gender"));
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter { PredictedLabelColumn = "PredictedLabel" });
+            var model = pipeline.Train<AgeRange, AgeRangePrediction>();
+            model.WriteAsync("AgeRangeModel.zip");
+            Console.ReadLine();
+        }
+   }
+}
