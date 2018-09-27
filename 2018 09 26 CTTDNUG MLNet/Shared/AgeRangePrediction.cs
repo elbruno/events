1c757f59a7f4995d61e0fc9b13998799199d2d63
El Bruno <elbruno@users.noreply.github.com>
2018-09-26 22:11:47 -0400

    add kitchener machine learning.net demos
    add kitchener machine learning.net demos

---
+++ 2018 09 26 CTTDNUG MLNet/Shared/AgeRangePrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace MLNetAvaLL.Shared
+{
+    public class AgeRangePrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public string PredictedLabel;
+    }
+}
