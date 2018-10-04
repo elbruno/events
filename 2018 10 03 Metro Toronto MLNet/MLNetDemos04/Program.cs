e4bf9a5b54c99dd582750ceb979ecad49dd97eef
El Bruno <elbruno@users.noreply.github.com>
2018-10-04 18:17:27 -0400

    add demos for 2018 10 04 Metro Toronto User Group - Introduction to Machine Learning.Net and Windows Machine Learning

---
+++ 2018 10 03 Metro Toronto MLNet/MLNetDemos04/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using MLNetAvaLL.Shared;
+
+namespace MLNetDemos04
+{
+    class Program
+    {
+        const string FileName = "AgeRangeData.csv";
+        const string OnnxPath = "SaveModelToOnnxTest.onnx";
+        const string OnnxAsJsonPath = "SaveModelToOnnxTest.json";
+        static void Main()
+        {
+            var pipeline = new LearningPipeline
+            {
+                new TextLoader(FileName).CreateFrom<AgeRange>(separator: ',', useHeader: true),
+                new Dictionarizer("Label"),
+                new TextFeaturizer("Gender", "Gender"),
+                new ColumnConcatenator("Features", "Age", "Gender"),
+                new StochasticDualCoordinateAscentClassifier(),
+                new PredictedLabelColumnOriginalValueConverter {PredictedLabelColumn = "PredictedLabel"}
+            };
+            var model = pipeline.Train<AgeRange, AgeRangePrediction>();
+
+            var converter = new OnnxConverter
+            {
+                Onnx = OnnxPath,
+                Json = OnnxAsJsonPath,
+                Domain = "com.elbruno"
+            };
+
+            converter.Convert(model);
+
+            // Strip the version.
+            var fileText = File.ReadAllText(OnnxAsJsonPath);
+            fileText = Regex.Replace(fileText, "\"producerVersion\": \"([^\"]+)\"", "\"producerVersion\": \"##VERSION##\"");
+            File.WriteAllText(OnnxAsJsonPath, fileText);
+        }
+    }
+}
---
+++ 2018 10 03 Metro Toronto MLNet/Shared/SentimentData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace MLDemos.Shared
+{
+    public class SentimentData
+    {
+        [Column("0")]
+        public string SentimentText;
+
+        [Column("1", name: "Label")]
+        public float Sentiment;
+    }
+}
