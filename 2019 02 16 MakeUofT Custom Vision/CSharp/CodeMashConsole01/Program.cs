180ae23c21632b78764af251becfdc2b816d367c
Bruno Capuano <bcapuano@gmail.com>
2019-02-18 11:23:12 -0500

    2019 02 16 Make UofT
    add samples from the event MakeUofT on Custom Vision in Windows 10 with Onnx and docker with Raspberry Pi

---
+++ 2019 02 16 MakeUofT Custom Vision/CSharp/CodeMashConsole01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using Newtonsoft.Json.Linq;
+using Formatting = Newtonsoft.Json.Formatting;
+
+namespace CodeMashConsole01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakePred("IMG_20190104_114712.jpg");
+            Console.ReadLine();
+        }
+
+        static async void MakePred(string imageFilePath)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Prediction-Key", "<Custom Vision Prediction Key>");
+            var url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v2.0/Prediction/<Custom Vision AppId>/image?iterationId=<Custom Vision Iteration Id>";
+            var byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                var response = await client.PostAsync(url, content);
+                var jsonResponse = await response.Content.ReadAsStringAsync();
+                var prettyJson = JToken.Parse(jsonResponse).ToString(Formatting.Indented);
+                Console.WriteLine(prettyJson);
+            }
+        }
+
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            var fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            var binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+    }
+}
---
+++ 2019 02 16 MakeUofT Custom Vision/Marvel CustomVision Docker RaspberryPi/Dockerfile
@@ -0,0 +1,49 @@
+﻿#FROM resin/raspberrypi3-python:3.6
+
+#ADD app /app
+
+#  pip upgrade
+#RUN pip3 install --upgrade pip
+
+#  pip install tensorflow
+#RUN pip3 install tensorflow>=1.0; exit 0
+
+#  install other requirements
+#RUN pip3 install -r /app/requirements.txt
+
+# Bruno changes
+FROM resin/rpi-raspbian:stretch
+
+#RUN [ "cross-build-start" ]
+
+# Install dependencies
+RUN apt-get update &&  apt-get install -y \
+        python3 \
+        python3-pip \
+        build-essential \
+        python3-dev \
+        libopenjp2-7-dev \
+        libtiff5-dev \
+        zlib1g-dev \
+        libjpeg-dev \
+        libatlas-base-dev \
+        wget
+
+COPY /build/arm32v7-requirements.txt arm32v7-requirements.txt
+
+RUN pip3 install --upgrade pip 
+RUN pip3 install --upgrade setuptools 
+RUN pip3 install -r arm32v7-requirements.txt
+
+ADD app /app
+
+# Expose the port
+EXPOSE 80
+
+# Set the working directory
+WORKDIR /app
+
+#RUN [ "cross-build-end" ]
+
+# Run the flask server for the endpoints
+CMD python app.py
