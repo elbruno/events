76d7db7149a87d782cf367baf3d0358fc432d208
El Bruno <elbruno@users.noreply.github.com>
2019-09-06 10:16:51 -0400

    global ai night demos

---
+++ 2019 09 05 Global AI Night/MLNetGlobalAINight01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace MLNetGlobalAINight01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mlContext = new MLContext(1);
+
+
+            Console.ReadLine();
+        }
+
+        public static void Predict(string text, PredictionEngine<SentimentIssue, SentimentPrediction> predEngine)
+        {
+            // prediction
+            var testSentiment = new SentimentIssue { Text = text };
+            var prediction = predEngine.Predict(testSentiment);
+
+            // Console Write
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Text: {testSentiment.Text}");
+            Console.ForegroundColor = prediction.Prediction ? ConsoleColor.Red : ConsoleColor.Green;
+            Console.WriteLine($"    Prediction: {(Convert.ToBoolean(prediction.Prediction) ? "Toxic" : "Good")} | Probability of being toxic: {prediction.Probability} ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+    public class SentimentIssue
+    {
+        [LoadColumn(0)]
+        public bool Label { get; set; }
+        [LoadColumn(2)]
+        public string Text { get; set; }
+    }
+    public class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
