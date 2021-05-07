176d81de5e4ca821184e7308aa3118a85c34ec56
El Bruno <bcapuano@gmail.com>
2021-05-07 17:15:26 -0400

    2021 05 07 Virtual ML.Net Drone Anomaly
    2021 05 07 Virtual ML.Net Drone Anomaly

---
+++ 2021 05 07 Virtual ML.Net Drone Anomaly/Anomalies CSharp/MLNetAnomalyDrone01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.ML;
+using SpikeDetection.DataStructures;
+
+namespace MLNetAnomalyDrone01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // define vars
+            var mlContext = new MLContext();
+            const int size = 150;
+
+            // load data
+            var dataView = mlContext.Data.LoadFromTextFile<DroneData>(path: "dronedata02.csv", hasHeader: true, separatorChar: ',');
+            var droneTelemetryList = new List<DroneData>();
+            var emptyDataView = mlContext.Data.LoadFromEnumerable(droneTelemetryList);
+
+            // Create Estimator and build model
+            var estimator = mlContext.Transforms.DetectIidSpike(outputColumnName: nameof(DronePrediction.Prediction), inputColumnName: nameof(DroneData.agx),confidence: 95, pvalueHistoryLength: size / 5);
+            var transformedModel = estimator.Fit(emptyDataView);
+
+            // Prediction
+            var transformedData = transformedModel.Transform(dataView);
+            var predictions = mlContext.Data.CreateEnumerable<DronePrediction>(transformedData, reuseRowObject: false);
+
+            // analyze predictions
+            Console.WriteLine("Alert\tScore\tP-Value");
+            foreach (var p in predictions)
+            {
+                if (p.Prediction[0] == 1)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.WriteLine("{0}\t{1:0.00}\t{2:0.00}", p.Prediction[0], p.Prediction[1], p.Prediction[2]);
+                Console.ResetColor();
+
+                //if (p.Prediction[0] == 1)
+                //{
+                //    Console.WriteLine("{0}\t{1:0.00}\t{2:0.00}", p.Prediction[0], p.Prediction[1], p.Prediction[2]);
+                //}
+            }
+        }
+    }
+}
---
+++ 2021 05 07 Virtual ML.Net Drone Anomaly/Anomalies CSharp/MLNetAnomalyDrone03/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.ML;
+using SpikeDetection.DataStructures;
+
+namespace MLNetAnomalyDrone03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // output data
+            var droneTelemetryCsv = "dronedata03.csv";
+            var droneTelemetryAnomaliesCsv = $"{droneTelemetryCsv}anomalies.csv";
+
+            var droneTelemetryLines = File.ReadAllLines(droneTelemetryCsv);
+
+            // define vars
+            var mlContext = new MLContext();
+            var size = droneTelemetryLines.Count() - 1;
+
+            // load data
+            var dataView = mlContext.Data.LoadFromTextFile<DroneData>(path: droneTelemetryCsv, hasHeader: true, separatorChar: ',');
+            var droneTelemetryList = new List<DroneData>();
+            var emptyDataView = mlContext.Data.LoadFromEnumerable(droneTelemetryList);
+
+            // Create Estimator and build model
+            var estimator = mlContext.Transforms.DetectIidChangePoint(
+                outputColumnName: nameof(DronePrediction.Prediction),
+                inputColumnName: nameof(DroneData.agx),
+                confidence: 90,
+                changeHistoryLength: size / 10);
+
+            var transformedModel = estimator.Fit(emptyDataView);
+
+            // Prediction
+            var transformedData = transformedModel.Transform(dataView);
+            var predictions = mlContext.Data.CreateEnumerable<DronePrediction>(transformedData, reuseRowObject: false);
+
+            // analyze predictions using Change Point Detection
+            var i = 0;
+            var droneTelemetryAnalysis = $"{droneTelemetryLines[i]},Alert,Score,P-Value,Martingale value,{Environment.NewLine}";
+            Console.Write(droneTelemetryAnalysis);
+            foreach (var pred in predictions)
+            {
+                var droneTelemetryLine = string.Format("{0},{1},{2:0.00},{3:0.00}, {4:0.00}, {5}", droneTelemetryLines[i + 1], pred.Prediction[0], pred.Prediction[1], pred.Prediction[2], pred.Prediction[3], Environment.NewLine); ;
+                droneTelemetryAnalysis += droneTelemetryLine;
+                i++;
+
+                if (pred.Prediction[0] == 1)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                Console.Write(droneTelemetryLine);
+                Console.ResetColor();
+            }
+
+            File.WriteAllText(droneTelemetryAnomaliesCsv, droneTelemetryAnalysis);
+        }
+    }
+}
