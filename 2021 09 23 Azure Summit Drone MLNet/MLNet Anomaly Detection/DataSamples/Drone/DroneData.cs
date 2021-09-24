90058b5155c421e758981526a5c397367ff1930d
El Bruno <bcapuano@gmail.com>
2021-09-24 08:15:27 -0400

    Cloud Summit Drone and MLNet demos

---
+++ 2021 09 23 Azure Summit Drone MLNet/MLNet Anomaly Detection/DataSamples/Drone/DroneData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+
+namespace SpikeDetection.DataStructures
+{
+    public class DroneData
+    {
+        [LoadColumn(0)]
+        public string DateTimeRecord;
+
+        [LoadColumn(1)]
+        public float agx;
+
+        [LoadColumn(2)]
+        public float agy;
+
+        [LoadColumn(2)]
+        public float agz;
+    }
+}
