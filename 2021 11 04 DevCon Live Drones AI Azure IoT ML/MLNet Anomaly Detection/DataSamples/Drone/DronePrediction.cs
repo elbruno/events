4cb1e4439efb2586e1c4829ab16f4e4b79446f87
El Bruno <bcapuano@gmail.com>
2021-11-04 10:20:14 -0400

    demo code for the event DevCon Live for the session Drones AI Azure IoT ML on 2021 11 04

---
+++ 2021 11 04 DevCon Live Drones AI Azure IoT ML/MLNet Anomaly Detection/DataSamples/Drone/DronePrediction.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.ML.Data;
+
+namespace SpikeDetection.DataStructures
+{
+    public class DronePrediction
+    {
+        //vector to hold alert,score,p-value values
+        [VectorType(4)]
+        public double[] Prediction { get; set; }
+    }
+}
